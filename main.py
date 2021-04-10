b88e5da56c969fa5476c8778a2f8c0dba33361de
3156 <nakamura@3156.bz>
2021-04-10 12:53:47 +0900

    createTile()をインスタンスから作成

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import math
 
 canvas = None
-tiles = []
 
 SQUARE_LENGTH = 100
 RADIUS = SQUARE_LENGTH / 2 - 5
@@ -15,17 +14,20 @@ LENGTH_Y = SQUARE_LENGTH * TILE_Y_COUNT + BORDER_WIDTH * TILE_Y_COUNT
 CELL_COLOR = '#cbbeb5'
 BORDER_COLOR = '#b2a698'
 
+tiles = []
+positions = [ [None] * TILE_X_COUNT ] * TILE_Y_COUNT
+
 class Tile:
   def __init__(self, x, y, number):
     self.x = x
     self.y = y
     self.number = number
 
-  def createTile(self, x, y):
-    center_x = POSITION["x"] + BORDER_WIDTH * x + BORDER_WIDTH / 2 + SQUARE_LENGTH * x + SQUARE_LENGTH / 2
-    center_y = POSITION["y"] + BORDER_WIDTH * y + BORDER_WIDTH / 2 + SQUARE_LENGTH * y + SQUARE_LENGTH / 2
+  def createTile(self, tile):
+    center_x = POSITION["x"] + BORDER_WIDTH * tile.x + BORDER_WIDTH / 2 + SQUARE_LENGTH * tile.x + SQUARE_LENGTH / 2
+    center_y = POSITION["y"] + BORDER_WIDTH * tile.y + BORDER_WIDTH / 2 + SQUARE_LENGTH * tile.y + SQUARE_LENGTH / 2
     canvas.create_rectangle(center_x - SQUARE_LENGTH / 2, center_y - SQUARE_LENGTH / 2, center_x + SQUARE_LENGTH / 2, center_y + SQUARE_LENGTH / 2, fill=CELL_COLOR, width=0)
-    canvas.create_text(center_x, center_y, text=0, justify="center", font=("", 70), tag="count_text")
+    canvas.create_text(center_x, center_y, text=tile.number, justify="center", font=("", 70), tag="count_text")
 
 def setTiles():
   global tiles
@@ -36,7 +38,7 @@ def setTiles():
 def createTiles():
   setTiles()
   for tile in tiles:
-    tile.createTile(tile.x, tile.y)
+    tile.createTile(tile)
 
 def set_field():
   canvas.create_rectangle(POSITION["x"], POSITION["y"], LENGTH_X + POSITION["x"], LENGTH_Y + POSITION["y"], fill='#cbbeb5', width=BORDER_WIDTH, outline=BORDER_COLOR)
